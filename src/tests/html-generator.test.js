82bd4fd6575cb9ea99ddebee011913ce21202c9c
Quinn1876 <qhodges0442@gmail.com>
2021-07-03 15:33:56 -0400

    testing framework

---
+++ src/tests/html-generator.test.js
@@ -0,0 +1,11 @@
+var expect = require("chai").expect;
+// import html generator
+describe("Html Generator", function() {
+  describe("async api", function() {
+    it("should ...", function() {
+      // call htmlgenerator
+      expect()
+    })
+  })
+
+})
---
+++ src/tests/test.test.js
@@ -0,0 +1,7 @@
+var expect = require("chai").expect;
+
+describe("Example Test", function() {
+  it("should be an example", function() {
+    expect("hello").to.equal("hello");
+  })
+})
